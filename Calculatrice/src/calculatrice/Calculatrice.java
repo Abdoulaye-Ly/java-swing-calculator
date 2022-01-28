cb90706155169440638884c6a109393b298f76d5
Abdoulaye Ly <abdoulayely536@gmail.com>
2022-01-28 19:31:11 +0100

    Add files via upload

---
+++ Calculatrice/src/calculatrice/Calculatrice.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculatrice;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author USER
+ */
+public class Calculatrice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame frm = new FrmCalculatrice();
+        frm.setVisible(true);
+    }
+    
+}
